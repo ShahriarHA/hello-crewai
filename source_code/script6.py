4837f9541913e7865b20c79051704b82bfaacf7e
ShahriarHA <shahriar08neub@gmail.com>
2026-03-29 19:55:28 +0600

    CrewAI--Flow-control-updated

--- source_code/script6.py
+++ source_code/script6.py
@@ -21,15 +21,17 @@ from crewai.flow.flow import Flow, listen, start, or_, and_
 class Flow_Control_and(Flow):
     @start()
     def startMethod(self):
-        return "Hello from start method"
+        # return "Hello from start method"
+        self.state["Greeting"] = "Hello from start method"
     
     @listen(startMethod)
     def secondMethod(self):
-        return "Hello2 from second Method"
+        # return "Hello2 from second Method"
+        self.state["joke"] = "What do computers eat? Microchips."
     
     @listen(and_(startMethod,secondMethod))
-    def logger(self,result):
-        print(result)
+    def logger(self):
+        print(self.state)
 
 f1 = Flow_Control_and()
 f1.plot()
